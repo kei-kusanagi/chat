50d0f9b3f59e78e9b1697eeae107e392ff040d0e
kei_kusanagi <kei_kusanagi@outlook.com>
2022-04-25 23:56:55 -0500

    commit
    Co-authored-by: Hector De Diego <lecksfrawen@gmail.com>

--- assets/js/elixir_chat_channel.js
+++ assets/js/elixir_chat_channel.js
@@ -1,33 +1,31 @@
-import { Push } from "phoenix"
-
 let ElixirChatChannel = {
     init(socket) {
-        let channel = socket.channel('elixir_chat_channel:lobby', {})
-        channel.join()
-        this.listenForChats(channel)
+        let channel = socket.channel('elixir_chat_channel:lobby', {});
+        channel.join();
+        this.listenForChats(channel);
     },
 
     listenForChats(channel) {
-        document.getElementById('chat-form').addEventListener('submit', function(e){
-            e.preventDefault()
+        document.getElementById('chat-form').addEventListener('submit', function(e) {
+            e.preventDefault();
 
-            let userName = document.getElementById('user-name').value
-            let userMsg = document.getElementById('user-msg').value
+            let userName = document.getElementById('user-name').value;
+            let userMsg = document.getElementById('user-msg').value;
 
-            channel-Push('shout', {name:userName,body: userMsg})
+            // channel-Push('shout', {name:userName,body: userMsg});
 
-            document.getElementById('user-name').value = ''
-            document.getElementById('user.msg').value = ''
-        })
+            document.getElementById('user-name').value = '';
+            document.getElementById('user-msg').value = ''; // Estaba como user.msg el selector
+        });
 
         channel.on('shout', payload => {
-            let chatBox = document.querySelector('#chat-box')
-            let msgBlock = document.createElement('p')
+            let chatBox = document.querySelector('#chat-box');
+            let msgBlock = document.createElement('p');
 
-            msgBlock.insertAdjacentHTML('beforeend', '<b>${payload.name}:</b> ${payload.body}')
-            chatBox.appendChild(msgBlock)
-        })
+            msgBlock.insertAdjacentHTML('beforeend', '<b>${payload.name}:</b> ${payload.body}');
+            chatBox.appendChild(msgBlock);
+        });
     }
 }
 
-export default ElixirChatChannel
+export default ElixirChatChannel;
